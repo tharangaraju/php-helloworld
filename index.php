2deed0953f122632d018b12602c78a2222a29d74
root <root@svr01.vsis-demo.local>
2021-03-13 11:04:01 +0530

    add index php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>';
+ print "php version" . PHP_VERSION . "\n";
+ print "A change is comming\n";
+ ?> 
+ </body>
+</html>
