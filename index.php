49a9fe9f73628ccc714ed7564a6c5ba2f3448eac
Tharanga Rathnayaka <66195882+tharangaraju@users.noreply.github.com>
2024-11-04 10:54:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
  print "php version" . PHP_VERSION . "\n</br>";
  print "temprature is 37.1 c </br>";
  print "hostname loadbalancer.5b37.example.opentlc.com</br>";
+ print "</br>"
+ print "<H1>HR system for Openshift</H1>";
  ?> 
  </body>
 </html>
