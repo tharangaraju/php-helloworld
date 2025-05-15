eb2bb6b17863f42adc92878dde2ec4f7f90adb26
tharangaraju <tharangaraju@gmail.com>
2025-05-15 09:23:04 +0300

    welcome change

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  <body>
  <?php echo '<p>Hello World</p>';
  print "php version" . PHP_VERSION . "\n</br>";
- print "temprature is 37.1 c </br>";
- print "hostname loadbalancer.5b37.example.opentlc.com</br>";
+ print "You are using PHP deployment checker page developed by Tharanga</br>";
+ print "You are welcome to modify this page</br>";
  print "</br>";
  print "HR system for Openshift</br>";
  ?> 
